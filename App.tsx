0811a9aa65a58426b2d2e138ff35f2e11f911aee
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-20 16:11:59 -0300

    pagina de Boas vindas quase pronta

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
   return (
-    <View style={style.container}>
-      <Text>NLW5</Text>
-    </View>
+    <Welcome />
   )
 }
-
-
-const style = StyleSheet.create({
-  container: {
-    flex:1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  }
-})
