c889d064b2689f15275a2cf9cb40a5e110ed6b0f
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-22 18:39:20 -0300

    expo-app-loading, segura na tela de loading até carregar o app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import { Welcome } from './src/pages/Welcome';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+    return(
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Guenta ai</Text>
+      </View>
+    )
+
   return (
     <Welcome />
   )
