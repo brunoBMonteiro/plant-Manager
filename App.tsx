5ad4e61702f9a64358584bd0d953a9987048f23b
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-22 22:34:49 -0300

    Componente do botão criado, tela de identificação criada, arquivo de estilos

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import AppLoading from 'expo-app-loading';
 
 import {
@@ -19,6 +19,6 @@ export default function App() {
     
 
   return (
-    <Welcome />
+    <UserIdentification />
   )
 }
---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { 
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    KeyboardAvoidingView,
+    Platform
+} from 'react-native';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { Button } from '../components/Button';
+
+
+export function UserIdentification() {
+    return(
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >    
+                <View style={styles.content}>
+                    <View style={styles.form}>
+                        <View style={styles.header}>    
+                            <Text style={styles.emoji}>
+                                😁
+                            </Text>
+                            <Text style={styles.title}>
+                                Como podemos{'\n'}
+                                chamar você?
+                            </Text>
+                        </View>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Digite seu nome aqui"
+                        />
+
+                        <View style={styles.footer}>    
+                            <Button/>
+                        </View>
+                    </View>
+                    
+                </View>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    content: {
+        flex: 1,
+        width: '100%'
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 54,
+        alignItems: 'center',
+    },
+    header: {
+        alignItems: 'center',
+    },
+    emoji: {
+        fontSize: 44
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.gray,
+        color: colors.heading,
+        width: '100%',
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center'
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        textAlign: 'center',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        marginTop: 20
+    },
+    footer: {
+        marginTop: 40,
+        width: '100%',
+        paddingHorizontal: 20
+    }
+})
